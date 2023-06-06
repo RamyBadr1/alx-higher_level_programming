2bc71c98d284fa7b4fea223295c49951e12b8491
RamyBadr1 <ramybadr054@gmail.com>
2023-06-07 02:32:42 +0300

    lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,7 +17,7 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-listint_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
