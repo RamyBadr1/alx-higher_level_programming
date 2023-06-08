2ab8dce09aedf9d743b5f316fa3560faa8322df4
RamyBadr1 <ramybadr054@gmail.com>
2023-06-08 22:29:06 +0300

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from hidden_4 import *
-    arr = dir()
-    for i in range(0, len(arr)):
-        if arr[i][0:2] != "__":
-            print("{}".format(arr[i]))
+
+    list = dir()
+
+    for i in range(0, len(list)):
+        if list[i][0:2] != "__":
+            print("{}".format(list[i]))
