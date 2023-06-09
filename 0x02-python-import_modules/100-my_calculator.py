401924431dc898aed73ee7edb35574c9fd895361
RamyBadr1 <ramybadr054@gmail.com>
2023-06-10 01:39:52 +0300

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 from calculator_1 import add, sub, mul, div
-
 if __name__ != "__main__":
     exit()
 
 argvLen = len(argv) - 1
-
-if argvLen < 3:
+if argvLen != 3:
     print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
 elif argv[2] == '+':
