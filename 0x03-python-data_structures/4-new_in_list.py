a5c597e140172626c88cd76c5ef2b2a41396c6b0
RamyBadr1 <ramybadr054@gmail.com>
2023-06-12 02:39:22 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 def new_in_list(my_list, idx, element):
     duplicate = my_list[:]
     if idx >= 0 and idx < len(duplicate):
-       duplicate[idx] = element
+        duplicate[idx] = element
     return duplicate
