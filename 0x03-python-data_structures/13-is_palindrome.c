d2241d7f7d0ed67ee7e8d35cdcecb57919743825
RamyBadr1 <ramybadr054@gmail.com>
2023-06-12 23:29:44 +0300

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+int check_palindrome(listint_t **head, listint_t *last);
+
+/**
+ * is_palindrome - to check if list is palindrome
+ * @head: pointer to list begining
+ * Return: 0 not palindrome || 1 palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if (!head || *head == NULL)
+		return (1);
+	return (check_palindrome(head, *head));
+}
+
+/**
+ * check_palindrome - check if the list is palindrome
+ * @head: pointer to list beginning
+ * @last: pointer to list end
+ * Return: 0 not palindrom || 1 palindrome
+ */
+int check_pal(listint_t **head, listint_t *last)
+{
+	if (!last)
+		return (1);
+
+	if (check_palindrome(head, last->next) != NULL && (*head)->n == last->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+
+	return (0);
+}
