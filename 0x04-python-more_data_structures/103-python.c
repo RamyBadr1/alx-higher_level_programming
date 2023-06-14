82a9baca3a8c46941275e636bf1cff1d6cda6f8b
RamyBadr1 <ramybadr054@gmail.com>
2023-06-15 02:21:17 +0300

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_python_bytes - gives data of the PyBytesObject
+ * print_python_bytes - prints data of the PyBytesObject
  * @p: the PyObject
  */
 
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size = 0, i = 0;
+	Py_ssize_t size = 0, index = 0;
 	char *string = NULL;
 
 	printf("[.] bytes object info\n");
@@ -24,17 +24,17 @@ void print_python_bytes(PyObject *p)
 		printf("  size: %zd\n", size);
 		printf("  trying string: %s\n", string);
 		printf("  first %zd bytes:", size < 10 ? size + 1 : 10);
-		while (i < size + 1 && i < 10)
+		while (index < size + 1 && index < 10)
 		{
-			printf(" %02hhx", string[i]);
-			i++;
+			printf(" %02hhx", string[index]);
+			index++;
 		}
 		printf("\n");
 	}
 }
 
 /**
- * print_python_list - gives data of the PyListObject
+ * print_python_list - prints data of the PyListObject
  * @p: the PyObject
  */
 
@@ -42,7 +42,7 @@ void print_python_list(PyObject *p)
 {
 	Py_ssize_t size = 0;
 	PyObject *item;
-	int i = 0;
+	int index = 0;
 
 	if (PyList_CheckExact(p))
 	{
@@ -52,13 +52,13 @@ void print_python_list(PyObject *p)
 		printf("[*] Size of the Python List = %zd\n", size);
 		printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
-		while (i < size)
+		while (index < size)
 		{
-			item = PyList_GET_ITEM(p, i);
-			printf("Element %d: %s\n", i, item->ob_type->tp_name);
+			item = PyList_GET_ITEM(p, index);
+			printf("Element %d: %s\n", index, item->ob_type->tp_name);
 			if (PyBytes_Check(item))
 				print_python_bytes(item);
-			i++;
+			index++;
 		}
 	}
 }
